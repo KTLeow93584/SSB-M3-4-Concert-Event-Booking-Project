2efe210df385bf4b062a80313753bec03ba546b2
Leow Kean Tat <leowkeantat@gmail.com>
2024-03-29 07:40:43 +0800

    Initial Commit. (With CRUD functionalities and vercel setup)

---
+++ src/App.jsx
@@ -0,0 +1,182 @@
+import { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
+import { Route, Routes, Outlet, useNavigate } from 'react-router-dom';
+import useLocalStorage from 'use-local-storage';
+
+import Container from 'react-bootstrap/Container';
+
+import Loader from './components/Loader.jsx';
+import NavigationPanel from './components/NavigationPanel.jsx';
+
+import GuestAuth from './auths/GuestAuth.jsx';
+import ParamsAuth from './auths/ParamsAuth.jsx';
+import UserAuth from './auths/UserAuth.jsx';
+
+import AuthPage from './pages/AuthPage.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import EditProfile from './pages/EditProfile.jsx';
+import NetErrorPage from './pages/NetErrorPage.jsx';
+
+import AddNewEvent from './pages/AddNewEvent.jsx';
+import ModifyEvent from './pages/ModifyEvent.jsx';
+// ==============================================
+
+import './App.css';
+
+import { store, persistor } from './store.jsx';
+import { sessionTokenStorageName, updateDeviceID, callServerAPI, updateSessionToken } from './apis/authApi.jsx';
+
+import { AuthProvider } from './contexts/AuthProvider.jsx';
+
+import { onLoadingStart, onLoadingEnd } from './data/loaders.js';
+import { errorNoAuthEventName, errorServerEventName } from './data/error-loggers.js';
+// =========================================
+export function Layout() {
+  return (
+    <>
+      <NavigationPanel />
+      <Loader />
+      <Outlet />
+    </>
+  );
+}
+// =========================================
+function App() {
+  // ================
+  const navigate = useNavigate();
+  // ================
+  const [deviceID, setDeviceID] = useLocalStorage("device-id", null);
+  if (!deviceID) {
+    const userAgentData = window.navigator.userAgentData;
+    const randomString = Math.random().toString(20).substring(2, 14) + Math.random().toString(20).substring(2, 14);
+
+    const deviceID = `${userAgentData.platform}-${userAgentData.brands[0].brand}-${randomString}`;
+    setDeviceID(deviceID);
+  }
+  updateDeviceID(deviceID);
+  // ================
+  // Verify Access Token at the beginning of the app.
+  useEffect(() => {
+    const accessToken = localStorage.getItem(sessionTokenStorageName, null);
+
+    const onUnauthorizationDetectedCallback = () => {
+      updateSessionToken(null);
+      navigate("/login");
+    };
+
+    const onServerErrorDetectedCallback = () => navigate("/error");
+
+    window.addEventListener(errorNoAuthEventName, onUnauthorizationDetectedCallback);
+    window.addEventListener(errorServerEventName, onServerErrorDetectedCallback);
+
+    if (accessToken) {
+      updateSessionToken(accessToken);
+
+      onLoadingStart("Global");
+      callServerAPI("whoami", "GET", null,
+        // On Successful Callback
+        (result) => {
+          onLoadingEnd("Global");
+
+          // Debug
+          //console.log("[Who Am I Verification Successful - Start of App Lifespan] Result.", result);
+
+          window.dispatchEvent(new CustomEvent("User Identified", {
+            detail: {
+              client_data: {
+                user: result.user
+              }
+            }
+          }));
+        },
+        // On Failed Callback
+        (error) => {
+          onLoadingEnd("Global");
+
+          // Debug
+          //console.log("[Who Am I Verification Failed - Start of App Lifespan] Error.", error);
+        }
+      );
+    }
+
+    return (() => {
+      window.removeEventListener(errorNoAuthEventName, onUnauthorizationDetectedCallback);
+      window.removeEventListener(errorServerEventName, onServerErrorDetectedCallback);
+    });
+  }, [navigate]);
+  // ================
+  return (
+    <Container fluid className="main-container m-0 p-0">
+      <AuthProvider>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                {/* ------------------------- */}
+                {/* Modify User Info Page */}
+                <Route element={
+                  <UserAuth>
+                    <EditProfile />
+                  </UserAuth>
+                } path="/profile/edit" />
+                {/* ------------------------- */}
+                {/* View Events Page */}
+                {/* ------------------------- */}
+                {/* Create Events Page */}
+                {/* ------------------------- */}
+                {/* Default Landing Page based on certain criterias */}
+                <Route index element={
+                  <UserAuth>
+                    <Dashboard />
+                  </UserAuth>
+                } />
+
+                <Route element={
+                  <UserAuth>
+                    <Dashboard />
+                  </UserAuth>
+                } path="/home" />
+
+                <Route element={
+                  <UserAuth>
+                    <Dashboard />
+                  </UserAuth>
+                } path="/dashboard" />
+
+                <Route element={
+                  <UserAuth>
+                    <AddNewEvent />
+                  </UserAuth>
+                } path="/event/add" />
+
+                <Route element={
+                  <UserAuth>
+                    <ParamsAuth>
+                      <ModifyEvent />
+                    </ParamsAuth>
+                  </UserAuth>
+                } path="/event/modify" />
+
+                <Route element={
+                  <GuestAuth>
+                    <AuthPage />
+                  </GuestAuth>
+                } path="/login" />
+
+                <Route element={
+                  <NetErrorPage />
+                } path="/error" />
+                {/* ------------------------- */}
+                <Route path="/*" element={<AuthPage />} />
+              </Route>
+            </Routes>
+          </PersistGate>
+        </Provider>
+      </AuthProvider>
+    </Container>
+  );
+}
+
+export default App
+// =========================================
